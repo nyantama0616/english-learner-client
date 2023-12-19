26161ba279a21a6d1be4f96d86b749ccbf7b36e5
nyantama <r_nyantama0616@yahoo.co.jp>
2023-12-19 21:12:41 +0900

    矢印キーで単語リストprev or nextできるようにした

--- src/features/word-list/hooks/useWordListHook.tsx
+++ src/features/word-list/hooks/useWordListHook.tsx
@@ -2,9 +2,12 @@ import IFetchWords from "../interfaces/IFetchWords"
 import { useEffect } from "react";
 import IWordListHook from "../interfaces/IWordListHook";
 import { useState } from "react";
+import { useHotkeys } from 'react-hotkeys-hook'
 
 export default function useWordList(fetchWords: IFetchWords): IWordListHook {
     const [selectedWordPos, setSelectedWordPos] = useState<number | null>(null);
+    useHotkeys('up', () => _selectPrevWord(), [selectedWordPos]); //TODO: 第２引数はどういう意味？, ショートカットを管理するクラスを作る
+    useHotkeys('down', () => _selectNextWord(), [selectedWordPos]);
 
     useEffect(() => {
         fetchWords.fetch();
@@ -12,6 +15,16 @@ export default function useWordList(fetchWords: IFetchWords): IWordListHook {
 
     function selectWord(pos: number) {
         setSelectedWordPos(pos);
+    }
+
+    function _selectPrevWord() {
+        if (selectedWordPos === null || selectedWordPos <= 0) return;
+        setSelectedWordPos(selectedWordPos - 1);
+    }
+
+    function _selectNextWord() {
+        if (selectedWordPos === null || selectedWordPos >= fetchWords.words.length - 1) return;
+        setSelectedWordPos(selectedWordPos + 1);
     }
 
     function closeWordViewer() {
