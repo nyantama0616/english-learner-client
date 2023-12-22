f3fcb9fc694cd85b6f7db4551efd11f9c49ad82f
nyantama <r_nyantama0616@yahoo.co.jp>
2023-12-22 16:45:24 +0900

    HomePageでpingのチェックできるようにした

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,14 +1,26 @@
 import { Box } from "@mui/material";
 import { SxProps } from "@mui/system";
 import PageTemplate from "../../general/PageTemplate";
-
+import { useEffect } from "react";
+import RequestManager from "../../general/classes/RequestManager";
+import requests from "../../general/requests";
 interface HomePageProps {
     sx?: SxProps;
 }
 export default function HomePage({ sx }: HomePageProps) {
+    const requestManager = new RequestManager<null, {message: string}>();
+    useEffect(() => {
+        requestManager
+            .get(requests.devs.ping, null)
+            .then(res => {
+                console.log(res);
+            });
+    }, []);
+
     return (
         <PageTemplate className="home-page" sx={sx}>
             <h1>Home</h1>
+
         </PageTemplate>
     )
 }
