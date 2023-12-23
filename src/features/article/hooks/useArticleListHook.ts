a6e9fdb163218880f265e35774268707225c7700
nyantama <r_nyantama0616@yahoo.co.jp>
2023-12-23 23:33:22 +0900

    矢印キーでArticleListを上下に選択できるようにした

--- src/features/article/hooks/useArticleListHook.ts
+++ src/features/article/hooks/useArticleListHook.ts
@@ -1,20 +1,30 @@
 import { useEffect, useState } from "react";
 import IArticleListHook from "../interfaces/IArticleListHook";
 import IFetchArticles from "../interfaces/IFetchArticles";
+import { useHotkeys } from "react-hotkeys-hook";
 
 export default function useArticleList(fetchArticles: IFetchArticles): IArticleListHook {
-    const [selectedArticlePos, setSelectedWordPos] = useState<number | null>(null);
+    const [selectedArticlePos, setSelectedArticlePos] = useState<number | null>(null);
+
+    useHotkeys('up', _selectPrevArticle, [selectedArticlePos]); //TODO: 第２引数はどういう意味？, ショートカットを管理するクラスを作る
+    useHotkeys('down', _selectNextArticle, [selectedArticlePos]);
 
     useEffect(() => {
         fetchArticles.fetch();
     }, []);
 
-    useEffect(() => {
-        console.log(selectedArticlePos);
-    }, [selectedArticlePos]);
-
     function selectArticle(pos: number | null) {
-        setSelectedWordPos(pos);
+        setSelectedArticlePos(pos);
+    }
+
+    function _selectPrevArticle() {
+        if (selectedArticlePos === null || selectedArticlePos <= 0) return;
+        selectArticle(selectedArticlePos - 1);
+    }
+
+    function _selectNextArticle() {
+        if (selectedArticlePos === null || selectedArticlePos >= fetchArticles.articles.length - 1) return;
+        selectArticle(selectedArticlePos + 1);
     }
     
     return {
